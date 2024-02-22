e75369c58b63d5838661477756011398fabede48
Roach83 <160878290+Roach83@users.noreply.github.com>
2024-02-22 22:39:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Import our newly installed setuptools package.
 import setuptools
 
-print("TEA OSS package")
+print("TEA-oss package-1")
 
 # Opens our README.md and assigns it to long_description.
 with open("README.md", "r") as fh:
