9e81a8a113b0f1a4346f72dc07b4af25c1cddc5f
Roach83 <160878290+Roach83@users.noreply.github.com>
2024-02-22 23:21:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import importlib.util
 from pathlib import Path
 from setuptools import setup, find_packages
 
-print("TEA-oss package-2")
+print("TEA-oss package-new")
 
 # Opens our README.md and assigns it to long_description.
 with open("README.md", "r") as fh:
